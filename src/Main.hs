24c5da85904d9b598c5995f157701017ef89ab14
Minh Nhat Vo <vnminh1999@gmail.com>
2022-11-17 00:20:30 -0800

    Starter code

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Brick
+import Graphics.Vty.Attributes
+import qualified Graphics.Vty as V
+import Brick.BChan (newBChan, writeBChan)
+import Control.Monad (forever)
+import Control.Concurrent (threadDelay, forkIO)
+
+-------------------------------------------------------------------------------
+main :: IO ()
+main = print("Hello")
