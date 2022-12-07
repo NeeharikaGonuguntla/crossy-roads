cc694194dd851be5dbae3855aa5123f9f7661f88
Neeharika <38551903+NeeharikaGonuguntla@users.noreply.github.com>
2022-12-06 23:56:32 -0800

    Added more tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -11,10 +11,13 @@ tests = TestList [TestLabel "test1" testUpdateScore1,
                   TestLabel "test3" testUpdateScore3,
                   TestLabel "test4" testCheckChicken1,
                   TestLabel "test5" testChangeState1,
-                  TestLabel "test6" testGrassCell,
-                  TestLabel "test7" testCarCell,
+                  TestLabel "test6" testUpdateRoads1,
+                  TestLabel "test7" testGetRoadsToDraw1,
                   TestLabel "test8" testGameState,
-                  TestLabel "test9" testGameState2]
+                  TestLabel "test9" testGameState2,
+                  TestLabel "test10" testGetRoadsToDraw2,
+                  TestLabel "test11" testChangeState2,
+                  TestLabel "test12" testUpdateRoads2]
 
 main = do
   runTestTT tests
